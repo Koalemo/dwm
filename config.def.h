8707122cd4a2090bf748a03278b627947f3aec57
Koalemo <gnoam@live.com>
2025-11-18 18:42:06 +0100

    attachbelow patch: Open new clients below the active one.

--- config.def.h
+++ config.def.h
@@ -38,6 +38,7 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
+static int       attachbelowactive = 1;    /* 1 means attach after curently active window*/
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -64,51 +65,52 @@ static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = {"qutebrowser"};
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } }, // open dmenu
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },  // open new terminal window
-	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },  // open new browser windwow
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, // monocle
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, // floating
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, // monocle
+	/* modifier                     key        function               argument */
+	{ MODKEY,                       XK_space,  spawn,                 {.v = dmenucmd } }, // open dmenu
+	{ MODKEY|ShiftMask,             XK_Return, spawn,                 {.v = termcmd } },  // open new terminal window
+	{ MODKEY,                       XK_w,      spawn,                 {.v = browsercmd } },  // open new browser windwow
+	{ MODKEY,                       XK_b,      togglebar,             {0} },
+	{ MODKEY,                       XK_j,      focusstack,            {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,            {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,            {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,            {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,              {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,              {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,                  {0} },
+	{ MODKEY,                       XK_Tab,    view,                  {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,            {0} },
+	{ MODKEY,                       XK_t,      setlayout,             {.v = &layouts[0]} }, // monocle
+	{ MODKEY,                       XK_f,      setlayout,             {.v = &layouts[1]} }, // floating
+	{ MODKEY,                       XK_m,      setlayout,             {.v = &layouts[2]} }, // monocle
 
-// { MODKEY,                       XK_space,  setlayout,      {0} },
-// { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-    { MODKEY|Mod1Mask,              XK_1,      focusbynum,     {.i = 0} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_2,      focusbynum,     {.i = 1} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_3,      focusbynum,     {.i = 2} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_4,      focusbynum,     {.i = 3} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_5,      focusbynum,     {.i = 4} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_6,      focusbynum,     {.i = 5} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_7,      focusbynum,     {.i = 6} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_8,      focusbynum,     {.i = 7} }, // Focus window within tag
- 	{ MODKEY|Mod1Mask,              XK_9,      focusbynum,     {.i = 8} }, // Focus window within tag
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+// { MODKEY,                       XK_space,  setlayout,              {0} },
+// { MODKEY|ShiftMask,             XK_space,  togglefloating,         {0} },
+	{ MODKEY,                       XK_0,      view,                  {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,                   {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,              {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,              {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,                {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,                {.i = +1 } },
+	TAGKEYS(                        XK_1,                             0)
+	TAGKEYS(                        XK_2,                             1)
+	TAGKEYS(                        XK_3,                             2)
+	TAGKEYS(                        XK_4,                             3)
+	TAGKEYS(                        XK_5,                             4)
+	TAGKEYS(                        XK_6,                             5)
+	TAGKEYS(                        XK_7,                             6)
+	TAGKEYS(                        XK_8,                             7)
+	TAGKEYS(                        XK_9,                             8)
+    { MODKEY|Mod1Mask,              XK_1,      focusbynum,            {.i = 0} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_2,      focusbynum,            {.i = 1} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_3,      focusbynum,            {.i = 2} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_4,      focusbynum,            {.i = 3} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_5,      focusbynum,            {.i = 4} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_6,      focusbynum,            {.i = 5} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_7,      focusbynum,            {.i = 6} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_8,      focusbynum,            {.i = 7} }, // Focus window within tag
+ 	{ MODKEY|Mod1Mask,              XK_9,      focusbynum,            {.i = 8} }, // Focus window within tag
+	{ MODKEY|ShiftMask,             XK_q,      quit,                  {0} },
+	{ MODKEY|ShiftMask,             XK_b,      toggleattachbelow,     {0} },
 };
 
 /* button definitions */
