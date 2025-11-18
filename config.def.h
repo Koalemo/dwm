00272c9d0ef252427786cc03ef842f90160fe502
Koalemo <gnoam@live.com>
2025-11-18 15:03:24 +0100

    Fix keybind conflict between new term and get focus

--- config.def.h
+++ config.def.h
@@ -63,8 +63,8 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } }, // open dmenu
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },  // open new terminal window
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } }, // open dmenu
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },  // open new terminal window
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -75,9 +75,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, // monocle
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, // floating
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, // monocle
+
 // { MODKEY,                       XK_space,  setlayout,      {0} },
 // { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
