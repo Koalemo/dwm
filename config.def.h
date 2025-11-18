7a0580883f27a43022ea6a45541231662e09baed
Koalemo <gnoam@live.com>
2025-11-18 17:02:12 +0100

    Add MOD-W open browser

--- config.def.h
+++ config.def.h
@@ -61,11 +61,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[] = {"qutebrowser"};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } }, // open dmenu
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },  // open new terminal window
+	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },  // open new browser windwow
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
