b714478b355d6744c0c0e843c461888edc45de4e
ng <noam.gallmann@gmail.com>
2026-01-22 03:19:00 +0100

    add inplacerotate

--- config.def.h
+++ config.def.h
@@ -89,6 +89,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,             {0} },
 	{ MODKEY,                       XK_j,      focusstack,            {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,            {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      inplacerotate,  {.i = +1} },
+	{ MODKEY|ShiftMask,             XK_k,      inplacerotate,  {.i = -1} },
 	{ MODKEY,                       XK_i,      incnmaster,            {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,            {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,                  {0} },
