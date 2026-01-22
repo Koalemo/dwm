12019ad610457a048909f39969a110d0afb511b9
ng <noam.gallmann@gmail.com>
2026-01-22 03:46:02 +0100

    add movestack: swatp windows in the stack with MOD-C-n and MOD-Shift-n

--- config.def.h
+++ config.def.h
@@ -94,6 +94,8 @@ static const Key keys[] = {
 	{ MODKEY|ControlMask,           XK_j,      inplacerotate,  {.i = +1} },
 	{ MODKEY|ControlMask,           XK_k,      inplacerotate,  {.i = -1} },
   { MODKEY,                       XK_n,      switchcol,   {0} },
+ 	{ MODKEY|ShiftMask,             XK_n,      movestack,      {.i = +1 } },
+ 	{ MODKEY|ControlMask,           XK_n,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,            {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,            {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,                  {0} },
